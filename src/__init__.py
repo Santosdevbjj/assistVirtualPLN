0b758035c099fa22f9f923c7e93a6bbdd4711de7
Sergio <santossergiorealbjj@outlook.com>
2025-09-21 07:47:36 -0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# assistVirtualPLN/src/__init__.py
+
+"""
+Módulo raiz do projeto de assistente virtual.
+
+Isso transforma a pasta 'src' em um pacote Python,
+permitindo que os submódulos sejam importados.
+"""
